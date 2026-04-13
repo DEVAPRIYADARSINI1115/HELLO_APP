6fa5320ded03fa44496a48aecfb698451cc7ee5f
DEVAPRIYADARSINI1115 <devapriyadarsinipinnamaneni@gmail.com>
2026-04-13 13:08:15 +0530

    Coded UC4: Display Hello with Multiple Command-Line Arguments

---
+++ src/main/java/HelloApp.java
@@ -0,0 +1,8 @@
+git checkout ;b ;feature=UC3-display-name default;public class HelloApp {
+    public static void main(String[] args) {
+
+        String name = (args.length > 0) ? args[0] : "World";
+
+        System.out.println("Hello, " + name + "!");
+    }
+}
